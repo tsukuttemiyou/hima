9ac6633a4b369923f41c910d8951138accd1084b
necota123 <necogoya@gmail.com>
2016-11-27 14:22:46 +0900

    まひ時の痺れを追加

--- Hima/Assets/biribiri.cs
+++ Hima/Assets/biribiri.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class biribiri : MonoBehaviour {
 
 	float time;
+	GameObject GO;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,13 @@ public class biribiri : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		Vector3 tempVec = gameObject.transform.position;
+		if (tempVec.x >= 0.0f) {
+			tempVec.x -= 0.2f;
+		} else {
+			tempVec.x += 0.2f;
+		}
+		gameObject.transform.position = tempVec;
 		if (time + 3.0f < Time.time) {
 			Destroy(gameObject);
 		}
